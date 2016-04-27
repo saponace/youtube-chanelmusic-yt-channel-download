b9377bf35ece21cb75dacc2986374cda6cf9beee
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-04-27 12:26:54 +0200

    Commented youtube-dl options

--- main.py
+++ main.py
@@ -6,27 +6,35 @@ import json
 
 ydl_opts = {
     'format': 'bestaudio/best',
+    # Post processor
     'postprocessors': [{
+        # Converter executable name
         'key': 'FFmpegExtractAudio',
+        # Prefered codec, if available
         'preferredcodec': 'mp3',
+        # Prefered quality, if available
         'preferredquality': '256',
     }],
+    # Do not print useless output
     'quiet': True,
+    # Download videos since
     'dateafter': 'now-10days',
+    # Do not extract videos from playlists
     'flatplaylist': True
     }
 
 ydl = youtube_dl.YoutubeDL(ydl_opts)
 result = ydl.extract_info(
-     'https://www.youtube.com/watch?v=nsIaibmkAbI',
+    # 'https://www.youtube.com/watch?v=nsIaibmkAbI',
+    'https://www.youtube.com/channel/UCohPRL-xLiPQqPt-hYNU44Q',
     download=False)
 
 
-json_result = json.loads(json.dumps(result))
 # Can be a playlist or a list of videos
 if 'entries' in result:
     print("Mock: create a playlist")
 
 # Just a video
 else:
+    json_result = json.loads(json.dumps(result))
     print("Title: " + json_result['title'])
