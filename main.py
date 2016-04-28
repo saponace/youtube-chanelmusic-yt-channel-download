269dcedd5cb679f0bb104b3523c750a9ebca4e54
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-04-28 14:13:45 +0200

    Cleanead some code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import youtube_dl
-from youtube_dl.utils import (DateRange)
 import json
 
 date_from = '2015042'
@@ -26,7 +25,9 @@ ydl_opts = {
 
 ydl = youtube_dl.YoutubeDL(ydl_opts)
 
-dict_result = ydl.extract_info(channel_url)
+dict_result = ydl.extract_info(channel_url,
+                               download=False
+                              )
 channel_content_dict = json.loads(json.dumps(dict_result))
 for item in channel_content_dict['entries']:
-    print("Downloaded: " + item['title'])
+    print("Downloaded: " + item['title'] + " uploaded at " + item['upload_date'])
