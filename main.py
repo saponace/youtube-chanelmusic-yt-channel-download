84b68e5c0d2de11cb8c5f7662d0024bfaf5bb4f0
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-04-28 13:58:48 +0200

    Instead of asking for a date since which to download videos, used built-in youtube-dl option downloaded_archive that maintain an archive file that contains the IDs of all the videos already downloaded so that they are not downloaded several times. That will be way easier to schedule this script in cron now. No need to store the last download date

--- main.py
+++ main.py
@@ -4,38 +4,29 @@ import youtube_dl
 from youtube_dl.utils import (DateRange)
 import json
 
-date_from = '20160422'
+date_from = '2015042'
+# channel_url = 'https://www.youtube.com/channel/UCohPRL-xLiPQqPt-hYNU44Q'
+channel_url = 'https://www.youtube.com/channel/UCJRljQ8OcyfzHBYpS_bDbow'
 
 
 ydl_opts = {
-    'format': 'bestaudio/best',
-    # Post processor
-    'postprocessors': [{
-        # Converter executable name
-        'key': 'FFmpegExtractAudio',
-        # Prefered codec, if available
-        'preferredcodec': 'mp3',
-        # Prefered quality, if available
-        'preferredquality': '256',
+    'format': 'bestaudio/best', # Best audio format
+    'postprocessors': [{  # Post processor
+        'key': 'FFmpegExtractAudio',  # Converter executable name
+        'preferredcodec': 'mp3',  # Prefered codec, if available
+        'preferredquality': '256',  # Prefered quality, if available
     }],
-    # Do not print useless output
-    # 'quiet': True,
-    # Download videos since
-    'daterange': DateRange(date_from, '99991231'),
-    # Do not extract videos from playlists
-    # 'extractflat': True
-    'simulate': True
+    # 'quiet': True,  # Do not print useless output
+    # 'verbose': True,  # Verbose
+    # 'simulate': True,  # Do not download the videos, but act as if
+    'download_archive': "download-archive.txt" # Maintain a archive file of all the
+                                                # already downloaded videos to avoid
+                                                # downloading them several times
     }
 
 ydl = youtube_dl.YoutubeDL(ydl_opts)
-result = ydl.extract_info('https://www.youtube.com/channel/UCohPRL-xLiPQqPt-hYNU44Q')
 
-
-# Can be a playlist or a list of videos
-if 'entries' in result:
-    print("Mock: create a playlist")
-
-# Just a video
-else:
-    json_result = json.loads(json.dumps(result))
-    print("Title: " + json_result['title'])
+dict_result = ydl.extract_info(channel_url)
+channel_content_dict = json.loads(json.dumps(dict_result))
+for item in channel_content_dict['entries']:
+    print("Downloaded: " + item['title'])
